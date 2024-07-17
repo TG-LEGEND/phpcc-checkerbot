6ad4497829fab7d440209bb697d176a827251aee
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2024-07-17 11:44:40 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@
 | It can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['botToken'] = "";
+$config['botToken'] = "7252195203:AAFEIYY0EKPUhaOJvIBoaqu0gFiRpWiyods";
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ $config['botToken'] = "";
 | ID can be obtained from https://telegram.dog/username_to_id_bot
 |
 */
-$config['adminID'] = "";
+$config['adminID'] = "5591734243";
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ $config['adminID'] = "";
 | ID can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['logsID'] =  "";
+$config['logsID'] =  "-1002200097211";
 
 /*
 |--------------------------------------------------------------------------
